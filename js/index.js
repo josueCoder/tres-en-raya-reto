e51a0712258b7568ec8bca966e17f97e21521365
rody josue <rody.josue.cc.c@gmail.com>
2022-02-22 13:49:21 -0500

    implementando funcion ponerFicha

--- js/index.js
+++ js/index.js
@@ -11,4 +11,35 @@ botones.forEach(
 	x => x.addEventListener("click", ponerFicha)
 );
 
+function ponerFicha(event){
+	let botonPulsado = event.target;
+	if(!partidaAcabada && botonPulsado.innerHTML == ""){
+		botonPulsado.innerHTML = fichas[turno];
+		puestas += 1;
+		
+		let estadoPartida = estado();
+		if(estadoPartida == 0){
+			cambiarTurno();
+			if(puestas < 9){
+				bot();
+				estadoPartida = estado();
+				puestas += 1;
+				cambiarTurno();	
+			}	
+		}
+		
+		if(estadoPartida == 1){
+			textoVictoria.style.visibility = "visible";
+			partidaAcabada = true;
+		}
+		else if(estadoPartida == -1){
+			textoVictoria.innerHTML = "😥Has perdido mi leader 😥"
+			partidaAcabada = true;
+			textoVictoria.style.visibility = "visible";
+		}
+	}	
+}
+
+
+
 
