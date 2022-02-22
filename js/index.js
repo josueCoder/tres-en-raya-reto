4f6c5f9158e485aad279c3cd6036b702b861be27
rody josue <rody.josue.cc.c@gmail.com>
2022-02-22 13:47:29 -0500

    declaracion de variables y enlazando elementos con js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
+let turno = 1;
+let fichas = ["O", "X"];
+let puestas = 0;
+let partidaAcabada = false;
+let textoVictoria = 
+	document.getElementById("textoVictoria");
+let botones = 
+	Array.from(document.getElementsByTagName("button"));
+
+botones.forEach(
+	x => x.addEventListener("click", ponerFicha)
+);
+
 
