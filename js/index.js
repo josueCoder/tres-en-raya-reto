ebd9f2f62c40e5dc94c472f9ee2d03135229eec7
rody josue <rody.josue.cc.c@gmail.com>
2022-03-01 18:15:43 -0500

    update la funcion estado

--- js/index.js
+++ js/index.js
@@ -1,41 +1,39 @@
-let turno = 1;
 let fichas = ["O", "X"];
-let puestas = 0;
-let partidaAcabada = false;
-let textoVictoria = 
-	document.getElementById("textoVictoria");
-let botones = 
-	Array.from(document.getElementsByTagName("button"));
+let turno = 1;
+let fichasPuestasTablero = 0;
+let endGame = false;
+let articleTextWinner = document.getElementById("articleTextWinner");
+let buttons = Array.from(document.getElementsByTagName("button"));
 
-botones.forEach(
+buttons.forEach(
 	x => x.addEventListener("click", ponerFicha)
 );
 
 function ponerFicha(event){
 	let botonPulsado = event.target;
-	if(!partidaAcabada && botonPulsado.innerHTML == ""){
+	if(!endGame && botonPulsado.innerHTML == ""){
 		botonPulsado.innerHTML = fichas[turno];
-		puestas += 1;
+		fichasPuestasTablero += 1;
 		
 		let estadoPartida = estado();
 		if(estadoPartida == 0){
 			cambiarTurno();
-			if(puestas < 9){
+			if(fichasPuestasTablero < 9){
 				bot();
 				estadoPartida = estado();
-				puestas += 1;
+				fichasPuestasTablero += 1;
 				cambiarTurno();	
 			}	
 		}
 		
 		if(estadoPartida == 1){
-			textoVictoria.style.visibility = "visible";
-			partidaAcabada = true;
+			articleTextWinner.style.visibility = "visible";
+			endGame = true;
 		}
 		else if(estadoPartida == -1){
-			textoVictoria.innerHTML = "😥Has perdido la partida 😥"
-			partidaAcabada = true;
-			textoVictoria.style.visibility = "visible";
+			articleTextWinner.innerHTML = "😥Has perdido la partida 😥"
+			endGame = true;
+			articleTextWinner.style.visibility = "visible";
 		}
 	}	
 }
@@ -54,10 +52,10 @@ function cambiarTurno(){
 
 const  estado=()=>{
 
-    posicionVictoria = 0;
+    positionVictory = 0;
 	nEstado = 0;
 
-	function sonIguales(...args){
+	function sonEquals(...args){
 		valores = args.map(x=>x.innerHTML);
 		if(valores[0] != "" && valores.every((x, i, arr) => x===arr[0])){
 			args.forEach(x => x.style.backgroundColor = "Turquoise")
@@ -69,40 +67,40 @@ const  estado=()=>{
 	}
 
 	//Comprobando todas las posibilidades de ganar
-	if(sonIguales(botones[0], botones[1], botones[2])){
-		posicionVictoria = 1;
+	if(sonEquals(buttons[0], buttons[1], buttons[2])){
+		positionVictory = 1;
 	}
 
-	else if(sonIguales(botones[3], botones[4], botones[5])){
-		posicionVictoria = 2;
+	else if(sonEquals(buttons[3], buttons[4], buttons[5])){
+		positionVictory = 2;
 	}
 
-	else if(sonIguales(botones[6], botones[7], botones[8])){
-		posicionVictoria = 3;
+	else if(sonEquals(buttons[6], buttons[7], buttons[8])){
+		positionVictory = 3;
 	}
 
-	else if(sonIguales(botones[0], botones[3], botones[6])){
-		posicionVictoria = 4;
+	else if(sonEquals(buttons[0], buttons[3], buttons[6])){
+		positionVictory = 4;
 	}
 
-	else if(sonIguales(botones[1], botones[4], botones[7])){
-		posicionVictoria = 5;
+	else if(sonEquals(buttons[1], buttons[4], buttons[7])){
+		positionVictory = 5;
 	}
 
-	else if(sonIguales(botones[2], botones[5], botones[8])){
-		posicionVictoria = 6;
+	else if(sonEquals(buttons[2], buttons[5], buttons[8])){
+		positionVictory = 6;
 	}
 
-	else if(sonIguales(botones[0], botones[4], botones[8])){
-		posicionVictoria = 7;
+	else if(sonEquals(buttons[0], buttons[4], buttons[8])){
+		positionVictory = 7;
 	}
 
-	else if(sonIguales(botones[2], botones[4], botones[6])){
-		posicionVictoria = 8;
+	else if(sonEquals(buttons[2], buttons[4], buttons[6])){
+		positionVictory = 8;
 	}
 
 	//comprobando quien gano
-	if(posicionVictoria > 0){
+	if(positionVictory > 0){
 		if(turno == 1){
 			nEstado = 1;
             
@@ -121,21 +119,21 @@ const bot=()=>{
         return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  let valores = botones.map(x=>x.innerHTML);
+  let valores = buttons.map(x=>x.innerHTML);
   let pos = -1;
 
   if(valores[4]==""){
       pos = 4;
   }
   else{
-      let n = aleatorio(0, botones.length-1);
+      let n = aleatorio(0, buttons.length-1);
       while(valores[n]!=""){
-          n = aleatorio(0, botones.length-1); 
+          n = aleatorio(0, buttons.length-1); 
       }
       pos = n;
   }
 
-  botones[pos].innerHTML = "O";
+  buttons[pos].innerHTML = "O";
   return pos;
 
 }
